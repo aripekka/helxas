0dfff45ea3485b6235bf821a5ff3ae54c9a5e1c1
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2018-07-11 15:15:26 +0300

    Refactored the scan reading part

--- helxas.py
+++ helxas.py
@@ -22,21 +22,20 @@ class HelXAS:
         correction = 1/(1-HelXAS.TAU_SCINTILLATOR*counts/counting_time)
         return counts*correction
 
-    def read_I0(self,signal_scan_numbers,background_scan_numbers):
+    def _read_scans(self,specfile,scan_numbers):
         '''
-        Loads the measured I0 scans into the memory
+        Internal method to read scans from the specfileself.
+
+        Input: SpecFile instance, list of scan_numbers
+        Output: dictionary containing the summed scans
         '''
-
-        specfile = SpecFile(os.path.join(self.datapath,self.datafile))
-
-        #Load I0
-        theta = specfile[str(signal_scan_numbers[0])+'.1'].data_column_by_name('Theta')
+        theta = specfile[str(scan_numbers[0])+'.1'].data_column_by_name('Theta')
 
         N0_raw = np.zeros(theta.shape) #raw counts
         N0 = np.zeros(theta.shape)     #deadtime corrected signal
         counting_time = np.zeros(theta.shape)   #counting time
 
-        for i in signal_scan_numbers:
+        for i in scan_numbers:
             detector = specfile[str(i)+'.1'].data_column_by_name('Detector')
             seconds = specfile[str(i)+'.1'].data_column_by_name('Seconds')
 
@@ -44,41 +43,27 @@ class HelXAS:
             N0 = N0 + self.scintillator_dead_time_correction(detector,seconds)
             counting_time = counting_time + seconds
 
-        self.direct_beam = {}
-        self.direct_beam['theta'] = theta
+        scans = {}
+        scans['scan_numbers'] = scan_numbers
+        scans['theta'] = theta
 
-        self.direct_beam['counts'] = N0
-        self.direct_beam['raw_counts'] = N0_raw
-        self.direct_beam['counts_error'] = np.sqrt(N0_raw)
-        self.direct_beam['counting_time'] = counting_time
+        scans['counts'] = N0
+        scans['raw_counts'] = N0_raw
+        scans['counts_error'] = np.sqrt(N0_raw)
+        scans['counting_time'] = counting_time
 
-        self.direct_beam['intensity'] = N0/counting_time
-        self.direct_beam['raw_intensity'] = N0_raw/counting_time
-        self.direct_beam['intensity_error'] = np.sqrt(N0_raw)/counting_time
+        scans['intensity'] = N0/counting_time
+        scans['raw_intensity'] = N0_raw/counting_time
+        scans['intensity_error'] = np.sqrt(N0_raw)/counting_time
 
-        #Load I0 background
-        theta_bg = specfile[str(background_scan_numbers[0])+'.1'].data_column_by_name('Theta')
+        return scans
 
-        N0_bg_raw = np.zeros(theta.shape) #raw counts
-        N0_bg = np.zeros(theta.shape)     #deadtime corrected signal
-        counting_time_bg = np.zeros(theta.shape)   #counting time
+    def read_I0(self,signal_scan_numbers,background_scan_numbers):
+        '''
+        Loads the measured I0 scans into the memory
+        '''
 
-        for i in background_scan_numbers:
-            detector = specfile[str(i)+'.1'].data_column_by_name('Detector')
-            seconds = specfile[str(i)+'.1'].data_column_by_name('Seconds')
+        specfile = SpecFile(os.path.join(self.datapath,self.datafile))
 
-            N0_bg_raw = N0_bg_raw + detector
-            N0_bg = N0_bg + self.scintillator_dead_time_correction(detector,seconds)
-            counting_time_bg = counting_time_bg + seconds
-
-        self.direct_beam_bg = {}
-        self.direct_beam_bg['theta'] = theta_bg
-
-        self.direct_beam_bg['counts'] = N0_bg
-        self.direct_beam_bg['raw_counts'] = N0_bg_raw
-        self.direct_beam_bg['counts_error'] = np.sqrt(N0_bg_raw)
-        self.direct_beam_bg['counting_time'] = counting_time_bg
-
-        self.direct_beam_bg['intensity'] = N0_bg/counting_time_bg
-        self.direct_beam_bg['raw_intensity'] = N0_bg_raw/counting_time_bg
-        self.direct_beam_bg['intensity_error'] = np.sqrt(N0_bg_raw)/counting_time_bg
+        self.direct_beam = self._read_scans(specfile,signal_scan_numbers)
+        self.direct_beam_bg = self._read_scans(specfile,background_scan_numbers)
